7b18a0409c66473ddc0d26f100a8da53523cb4cd
Theo S <iosdeveloper@tgs.me.uk>
2026-02-22 12:29:27 +0000

    add Bundle Helper to expose module publicly

---
+++ Sources/Clp/BundleHelper.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  DataSample
+//
+//  Created by Theo S   on 22/02/2026.
+//
+
+import Foundation
+
+import Foundation
+
+/// Public helper that gives callers access to the bundle that contains the test data.
+public enum DataSample {
+    public static var bundle: Bundle { .module }
+}
+
